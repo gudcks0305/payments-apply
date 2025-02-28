2830918521c21e13fc8b3e055f9bb48bdfe23ed3
yuhyeongchan <gudcks305@gmail.com>
2025-02-28 15:39:52 +0900

    chore : 필요없는 boilerplate 코드 제거

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/gudcks0305/payments-apply/internal/api"
 	"github.com/gudcks0305/payments-apply/internal/config"
-	"github.com/gudcks0305/payments-apply/internal/middleware"
 	"github.com/gudcks0305/payments-apply/internal/portone"
 	"github.com/gudcks0305/payments-apply/pkg/logger"
 	"go.uber.org/fx"
@@ -22,7 +21,6 @@ func main() {
 		// 의존성 제공
 		fx.Provide(
 			config.NewConfig,
-			middleware.NewLoggerMiddleware,
 			logger.NewLogger,
 			portone.NewClient,
 			api.NewRouter,
