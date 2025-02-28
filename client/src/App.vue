8e9c48996c51ec232fe1a8eaa6a6721959ee3947
yuhyeongchan <gudcks305@gmail.com>
2025-02-28 16:27:35 +0900

    client vue client 제작 및 결제 컴포넌트 제작

---
+++ client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="container">
+    <header>
+      <h1>PortOne 결제 시스템</h1>
+    </header>
+    <PaymentForm />
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import PaymentForm from './components/PaymentForm.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    PaymentForm
+  }
+})
+</script>
+
+<style>
+header {
+  text-align: center;
+  margin-bottom: 2rem;
+  padding-top: 2rem;
+}
+</style>
